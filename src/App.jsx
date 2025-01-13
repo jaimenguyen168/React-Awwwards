df67dea6f891d4585986afcaff948b922836859e
Jaime <nguyenphuocdat168@gmail.com>
2025-01-12 23:45:52 -0500

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <h1 className="text-5xl text-violet-300 font-robert-medium">
+                Hello Vite!
+            </h1>
+        </main>
     )
 }
 export default App
