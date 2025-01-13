2e3b9218dfef157cca7add5e75b6552b4e58ed66
Jaime <nguyenphuocdat168@gmail.com>
2025-01-13 01:07:21 -0500

    animate videos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React, {useRef, useState} from 'react'
 import Button from "./Button.jsx";
 import { TiLocationArrow } from "react-icons/ti";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
 
@@ -23,6 +25,27 @@ const Hero = () => {
     }
 
     const getVideoSource = (index) => `videos/hero-${index}.mp4`
+
+    useGSAP(() => {
+        if (hasClicked) {
+            gsap.set("#next-video", {visibility: "visible"})
+            gsap.to("#next-video", {
+                transformOrigin: "center center",
+                scale: 1,
+                width: "100%",
+                height: "100%",
+                duration: 1,
+                ease: "power1.inOut",
+                onStart: () => nextVideoRef.current.play(),
+            });
+            gsap.from("#current-video", {
+                transformOrigin: "center center",
+                scale: 0,
+                duration: 1.5,
+                ease: "power1.inOut",
+            });
+        }
+    }, {dependencies: [currentIndex], revertOnUpdate: true})
 
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
